a81bff84083d7cda37c03672206dc96eb5cd11eb
Asus <vaxa2320@gmail.com>
2024-06-09 15:20:38 +0300

    scan_1_12

--- src/src/attack.c
+++ src/src/attack.c
@@ -1,9 +1,3 @@
-/* SPDX-FileCopyrightText: 2023 Greenbone AG
- * SPDX-FileCopyrightText: 2006 Software in the Public Interest, Inc.
- * SPDX-FileCopyrightText: 1998-2006 Tenable Network Security, Inc.
- *
- * SPDX-License-Identifier: GPL-2.0-only
- */
 
 
 #include "attack.h"
